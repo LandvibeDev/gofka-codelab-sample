429b72a8182b2ce3c9152a539b71f708ec3a5213
KeonHee <kimkh6554@gmail.com>
2019-07-16 00:52:29 +0900

    connect mongodb

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // User
@@ -14,7 +19,7 @@ type User struct {
 	Email string `json:"email" form:"email" query:"email"`
 }
 
-var UserDB map[string]User = make(map[string]User)
+var collection *mongo.Collection
 
 func main() {
 	// Echo instance
@@ -23,6 +28,21 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	collection = client.Database("gofka").Collection("users")
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
@@ -40,32 +60,41 @@ func main() {
 
 // e.POST("/users", saveUser)
 func saveUser(c echo.Context) error {
+	ctx := c.Request().Context()
+
 	u := new(User)
 	if err := c.Bind(u); err != nil {
 		return err
 	}
 
-	if _, ok := UserDB[u.ID]; !ok {
-		UserDB[u.ID] = *u
+	insertResult, err := collection.InsertOne(ctx, u)
+	if err != nil {
+		return err
 	}
 
+	fmt.Println("Inserted a single documents: ", insertResult.InsertedID)
 	return c.JSON(http.StatusCreated, u)
 }
 
 // e.GET("/users/:id", getUser)
 func getUser(c echo.Context) error {
+	ctx := c.Request().Context()
+
 	// User ID from path `users/:id`
 	id := c.Param("id")
 
-	if user, ok := UserDB[id]; ok {
-		return c.JSON(http.StatusOK, user)
-	} else {
+	var user User
+	if err := collection.FindOne(ctx, bson.D{{"id", id}}).Decode(&user); err != nil {
+		fmt.Println(id+" not exist: ", err)
 		return c.String(http.StatusNotFound, id+" not exist")
 	}
+
+	return c.JSON(http.StatusOK, user)
 }
 
 // e.PUT("/users/:id", updateUser)
 func updateUser(c echo.Context) error {
+	ctx := c.Request().Context()
 	// User ID from path `users/:id`
 	id := c.Param("id")
 
@@ -74,25 +103,31 @@ func updateUser(c echo.Context) error {
 		return err
 	}
 
-	if _, ok := UserDB[id]; ok {
-		UserDB[id] = *u
-	} else {
+	updatedResult, err := collection.ReplaceOne(ctx, bson.D{{"id", id}}, u)
+	if err != nil || updatedResult == nil {
+		return err
+	}
+
+	if updatedResult.MatchedCount == 0 {
 		return c.String(http.StatusNotFound, id+" not exist")
 	}
 
+	fmt.Println("Updated a single documents: ", updatedResult.UpsertedID)
 	return c.JSON(http.StatusOK, u)
 }
 
 // e.DELETE("/users/:id", deleteUser)
 func deleteUser(c echo.Context) error {
+	ctx := c.Request().Context()
+
 	// User ID from path `users/:id`
 	id := c.Param("id")
 
-	if _, ok := UserDB[id]; ok {
-		delete(UserDB, id)
-	} else {
-		return c.String(http.StatusNotFound, id+" not exist")
+	deletedResult, err := collection.DeleteOne(ctx, bson.D{{"id", id}})
+	if err != nil {
+		return err
 	}
 
-	return c.String(http.StatusOK, id+" is deleted")
+	fmt.Println("Deleted documents count: ", deletedResult.DeletedCount)
+	return c.String(http.StatusNoContent, id+" is deleted")
 }
